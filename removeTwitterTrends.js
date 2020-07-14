62ca8662982a5be12679354303333d84c6b76a85
ssdh233 <songsongdahu233@gmail.com>
2020-07-14 21:16:21 +0900

    Call it periodically (Didn't find a way to listen to url change)

--- removeTwitterTrends.js
+++ removeTwitterTrends.js
@@ -1,16 +1,20 @@
-function removeTwitterTrends(callback) {
-  console.log("Trying to removing Twitter Trends");
-  const div = document.querySelector(
-    `div[aria-label="タイムライン: トレンド"]`
-  );
-  if (div) {
-    div.style.display = "none";
-    callback();
+function removeTwitterTrends() {
+  function tryToRemove(callback) {
+    console.log("Trying to removing Twitter Trends");
+    const div = document.querySelector(
+      `div[aria-label="タイムライン: トレンド"]`
+    );
+    if (div) {
+      div.style.display = "none";
+      callback();
+    }
   }
+
+  let whatever = setInterval(() => {
+    tryToRemove(() => {
+      clearInterval(whatever);
+    });
+  }, 100);
 }
 
-let whatever = setInterval(() => {
-  removeTwitterTrends(() => {
-    clearInterval(whatever);
-  });
-}, 100);
+setInterval(() => removeTwitterTrends(), 100);
